b09032310747b0e186367e0629c5b3aca8539e93
Christopher Thomas <cthomas@netsertive.com>
2020-03-13 15:03:14 -0700

    Updates

---
+++ precache-manifest.e282a49d9df4a07fd5051a8355897a52.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56506c773f55a47e081f6bbd32409cf0",
+    "url": "/cardistry/index.html"
+  },
+  {
+    "revision": "7856539f745cadf86020",
+    "url": "/cardistry/static/css/2.6000eced.chunk.css"
+  },
+  {
+    "revision": "b9b65fbdae9251031dd7",
+    "url": "/cardistry/static/css/main.8f41f661.chunk.css"
+  },
+  {
+    "revision": "7856539f745cadf86020",
+    "url": "/cardistry/static/js/2.f3dfdc48.chunk.js"
+  },
+  {
+    "revision": "6cede3e9e7bec0bf81a1b4f6826000aa",
+    "url": "/cardistry/static/js/2.f3dfdc48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9b65fbdae9251031dd7",
+    "url": "/cardistry/static/js/main.113145d1.chunk.js"
+  },
+  {
+    "revision": "e27ca246cbf9b1f36868",
+    "url": "/cardistry/static/js/runtime-main.33032e9b.js"
+  },
+  {
+    "revision": "5a64882a05434beed4c8004bd887d55a",
+    "url": "/cardistry/static/media/basic-shield.5a64882a.svg"
+  },
+  {
+    "revision": "9530ca7683c571718fe8a00a0dfabae6",
+    "url": "/cardistry/static/media/fight.9530ca76.svg"
+  }
+]);
